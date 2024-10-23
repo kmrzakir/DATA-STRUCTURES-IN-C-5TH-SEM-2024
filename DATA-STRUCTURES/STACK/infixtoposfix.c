887239c54ec836c8625fa36f8b87965dfe6be006
zakir <zakirmustafa444@gmaol.com>
2024-10-23 10:25:15 +0530

    added commits in infix to posfix conversion

--- DATA-STRUCTURES/STACK/infixtoposfix.c
+++ DATA-STRUCTURES/STACK/infixtoposfix.c
@@ -12,10 +12,11 @@ int main(){
 
    printf("Enter an expression : ");
    fgets(infix,99,stdin);
-   printf("The infix expression is %s : ",infix);
+   printf("The infix expression is %s ",infix);
      
+     // This will convert infix exp into posfix
    infoxToPosfix();
-   printf("Posfix expression is %s : ",posfix);
+   printf("Posfix expression is %s  ",posfix);
     return 0;
 }
 
@@ -23,19 +24,29 @@ int main(){
 void infoxToPosfix(){
     char symbol,popelm;
     int p = 0;
+    // Run the loop for the length of an infix  exp
     for(int i = 0;infix[i] != '\0';i++){
      symbol = infix[i];
+     
+     // If '(' comes then push in stack
      if(symbol == '('){
         push(symbol);
-     }else if (symbol == ')')
+     }
+     
+     // If '(' comes then pop all operators from stack until we find '('
+     else if (symbol == ')')
      {
         while ((popelm = pop()) != '(')
         {
             posfix[p++] = popelm;
         }
         
-     }else if (symbol == '*' || symbol == '+' || symbol == '/' || symbol == '-' || symbol == '^')
+     }
+     
+     // If we find any operator
+     else if (symbol == '*' || symbol == '+' || symbol == '/' || symbol == '-' || symbol == '^')
      {
+        // Check the presedence of an operator
         while (isEmpty() == 1 && presidence(symbol) <= presidence(stack[top]) && stack[top] != '(' && stack[top] != ')')
         {
             char tt = 
@@ -44,19 +55,26 @@ void infoxToPosfix(){
         }
         push(symbol);
      }
+
+     // This default will handle operand's and will put into the posfix expression
      else{
         posfix[p++] = symbol;
      }
     }
+
+    // After scanning entire infix expression this will pop all the remaning operator's in the stack and will put in posfix exp
     while (top != -1)
     {
         popelm = pop();
         posfix[p++] = popelm;
     }
     
+    // This will add '\0' at bthen the end of posfix expression which if help full for various perpose's
     posfix[p] = '\0';
 }
 
+
+// Push operator in stack
 void push(char operator){
     if(top == MAX){
         printf("Stack is full can't push further operator's : \n");
@@ -66,6 +84,7 @@ void push(char operator){
     stack[top] = operator;
 }
 
+// Pop operator from the satck
 char pop(){
     if(top == -1){
         printf("Stack is empty can't pop operator : \n");
@@ -77,6 +96,8 @@ char pop(){
     }
    
 }
+
+// Check is stack empty or not
 int isEmpty(){
     if(top == -1){
         return 0;
@@ -85,6 +106,7 @@ int isEmpty(){
     }
 }
 
+// This will set the presedence of operators
 int presidence(char operator){
  switch (operator)
   {
@@ -100,102 +122,3 @@ int presidence(char operator){
     return 1;
   }
 }
-
-// If the incoming operator has the higher Precedence  then operator present in stack then simply push the operator in the stack.
-
-// void infixToPosfix();
-// int presidence(char symbol);
-// int isEmpty();
-// void push(char operator);
-// char pop();
-// int main(){
-
-// printf("Enter the posfix expression : ");
-// fgets(infix, MAX-1,stdin);
-// printf("The infix expression is %s ",infix);
-
-// infixToPosfix();
-
-//     return 0;
-// }
-
-// void infixToPosfix(){
-//   int p = 0;
-//        for(int i = 0; infix[i] != '\0';i++){
-//         char symbol = infix[i],next;
-//               switch (symbol)
-//               {
-//               case  '(':
-//                 push(symbol);
-//                 break;
-//               case ')' : {
-//                 next = pop();
-//                 while (!isEmpty() && next != '('){
-//                        posfix[p++] = next; 
-//                        next = pop();
-//                 }
-//               }
-//               case '*':
-//               case '^':
-//               case '+':
-//               case '-':
-//               case '/':{
-//                 while (!isEmpty() && presidence(symbol) <= presidence(stack[top]))
-//                 {
-//                    next = pop();
-//                    posfix[p++] = next;
-//                    break;
-//                 }
-//                push(symbol);
-//                 break;
-//               }
-//               default:
-//               posfix[p++] = symbol;
-//                 break;
-//               }
-//        }
-//        printf("Postfix expression is : \n");
-//        for(int i = 0;i<=p;i++){
-//         printf("%c",posfix[i]);
-//        }
-// }
-// // This function will set the presidence to the operator's
-// int presidence(char symbol){
-//   switch (symbol)
-//   {
-//   case '^':
-//     return 3;
-//   case '*':
-//   case '/':
-//   return 2;
-//   default:
-//     return 1;
-//   }
-// }
-
-// int isEmpty(){
-//   if(top == -1){
-//     return 0;
-//   }else{
-//     return 1;
-//   }
-// }
-
-// void push(char operator){
-//   if(top == MAX){
-//     printf("Stack is full can't push further operator's");
-//   }else{
-//       top++;
-//       stack[top] = operator;
-//   }
-// }
-
-// char pop(){
-//   if(top == -1){
-//     printf("Stack is empty can't pop operator");
-//   }else{
-//     int operator = stack[top];
-//     top--;
-//     return operator;
-//   }
-// }
